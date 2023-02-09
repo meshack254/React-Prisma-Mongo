900821a3646575e911989e9a76a203a413074da3
meshack254 <meshackmutinda555@gmail.com>
2023-02-09 13:23:36 +0300

    Made a read request and mapped the results in React frontend.

--- Backend/index.js
+++ Backend/index.js
@@ -1,24 +1,46 @@
 const express = require("express");
 const cors = require("cors");
-const acios = require("axios")
+const acios = require("axios");
 //Prisma
-import { PrismaClient } from '@prisma/client'
-const prisma = new PrismaClient()
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
 
 const app = express();
-require("dotenv").config()
+require("dotenv").config();
 
 app.use(cors());
 app.use(express.json());
 
-async function main() {
+
+//Create request
+
+
+//Read request
+app.get("/user", (req, res) => {
+  async function main() {
     await prisma.$connect();
-}
+    const users = await prisma.user.findMany();
+    res.send(users)
+    console.log(users);
+  }
 
-const server = app.listen(process.env.PORT, ()=> {
-    console.log("Server running");
-    app.get("")
-})
+  main()
+    .then(async () => {
+      await prisma.$disconnect();
+    })
+    .catch(async (e) => {
+      console.log(e);
+      await prisma.$disconnect();
+      process.exit();
+    });
+});
+
+//Update request
+
+//Delete request
 
 
-// https://8080-meshack254-reactprisma-h4ku1st3dri.ws-eu85.gitpod.io
+const server = app.listen(process.env.PORT, () => {
+  console.log("Server running");
+});
+
--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -6,46 +6,68 @@ import {
 import { Avatar, Card } from "antd";
 import { Button, Space } from "antd";
 import axios from "axios";
+import { useEffect, useState } from "react";
 
 const { Meta } = Card;
 
 const App = () => {
+  const [posts, setPosts] = useState([]);
+  const [getPost, setGetPost] = useState();
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:8080/user", () => {
+        params: {
+          email: "doe@gmail.com";
+        }
+      })
+      .then((response) => {
+        setPosts(response.data);
+        console.log(posts[0]);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
   const submitBtn = () => {
-    axios.post("", {
-      name: "Jane Doe",
-      email: "doe@gmail.com"
-    })
+    // axios.post("", {
+    //   name: "Jane Doe",
+    //   email: "doe@gmail.com"
+    // })
   };
   return (
     <>
-      <Card
-        style={{
-          width: 300,
-        }}
-        cover={
-          <img
-            alt="example"
-            src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
-          />
-        }
-        actions={[
-          <SettingOutlined key="setting" />,
-          <EditOutlined key="edit" />,
-          <EllipsisOutlined key="ellipsis" />,
-        ]}
-      >
-        <Meta
-          avatar={<Avatar src="https://joeschmoe.io/api/v1/random" />}
-          title="Card title"
-          description="This is the description"
-        />
-      </Card>
-      <Space>
-        {" "}
-        <Button type="primary" onClick={submitBtn}>
-          Primary Button
-        </Button>
-      </Space>
+      {posts.map((postContent) => {
+        return (
+          <section className="cards">
+            <Card
+              style={{
+                width: 300,
+              }}
+              cover={
+                <img
+                  alt="example"
+                  src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
+                />
+              }
+              actions={[
+                <SettingOutlined key="setting" />,
+                <EditOutlined key="edit" />,
+                <EllipsisOutlined key="ellipsis" />,
+              ]}
+            >
+              <Meta
+                avatar={
+                  <Avatar src="https://ui-avatars.com/api/?name=John+Doe" />
+                }
+                title={postContent.name}
+                description={postContent.email}
+              />
+            </Card>
+          </section>
+        );
+      })}
     </>
   );
 };
